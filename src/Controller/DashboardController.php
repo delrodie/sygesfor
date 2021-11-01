28b653e730bf5f2c436e120a23026158677f4e06
delrodie <delrodieamoikon@gmail.com>
2021-11-01 18:24:05 +0000

    Backend Dashboard

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @route("/conafor")
+ */
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/", name="conafor_dashbord")
+     */
+    public function index(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
